c75658deaee1d1ab6d63836505ff2a21a1955abc
dapkunayte <gladiatormahotina@yandex.ru>
2021-04-06 13:16:51 +0700

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./style.css";
-import cat from './cat2.png'
+import cat from "./cat2.png";
 
 export default function App() {
   return (
     <div>
-    <html lang="ru"></html>
+    <html lang="ru"/>
     <head>
     <meta charset="utf-8"></meta>
     <title>Привет всем присутствующим</title>
-    <link rel="stylesheet" href="style.css"></link>
-    <link rel="shortcut icon" src="cat_icon.png"></link>
-    <meta name="viewport" content="width=device-width"></meta>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="shortcut icon" src="cat_icon.png"/>
+    <meta name="viewport" content="width=device-width"/>
     </head>
 
     <header class="site-header">
       <div class="container">
         <h1>УЧИМСЯ ВЕРСТАТЬ САЙТЫ</h1>
         <div class="imgpos">
-        <img class="cat" src={cat} width="510" height="510" alt="Кот"></img>
+        <img class="cat" src={cat} width="510" height="510" alt="Кот"/>
         </div>
       </div>
     </header>
 
         <div class="info">
-    <img class="back" src="imgs/Frame.png"></img>
+    <img class="back" src="imgs/Frame.png"/>
     <a target="_blank" class="button" href="https://yandex.ru/images/search?from=tabbar&text=рыжие%20коты">БОЛЬШЕ</a>
     </div>
 
