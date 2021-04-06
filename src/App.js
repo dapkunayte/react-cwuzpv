40ea96eeabc547a4d09a2735ccccf7d6413bf127
dapkunayte <gladiatormahotina@yandex.ru>
2021-04-06 10:53:00 +0700

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import "./style.css";
 
 export default function App() {
   return (
-
+    <div>
     <header class="site-header">
       <div class="container">
         <h1>УЧИМСЯ ВЕРСТАТЬ САЙТЫ</h1>
         <div class="imgpos">
-        <img class="cat" src="cat2.png" width="510" height="510" alt="Кот">
+        <img class="cat" src="cat2.png" width="510" height="510" alt="Кот"></img>
         </div>
       </div>
     </header>
-    
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+
+        <div class="info">
+    <img class="back" src="Frame.png"></img>
+    <a target="_blank" class="button" href="https://yandex.ru/images/search?from=tabbar&text=рыжие%20коты">БОЛЬШЕ</a>
     </div>
-  );
-}
+
+    <footer>
+    <a target="_blank" href="https://icons8.ru/icons/set/cat-butt">Cat Butt icon</a> иконка от <a target="_blank" href="https://icons8.ru">Icons8</a>
+    </footer>
+    </div>
+  )
+  
+
+};
+
+
