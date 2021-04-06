1a6133cbca9dbe431eed6962116e2deb1cff0975
dapkunayte <gladiatormahotina@yandex.ru>
2021-04-06 10:38:36 +0700

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import "./style.css";
 
 export default function App() {
   return (
+
+    <header class="site-header">
+      <div class="container">
+        <h1>УЧИМСЯ ВЕРСТАТЬ САЙТЫ</h1>
+        <div class="imgpos">
+        <img class="cat" src="cat2.png" width="510" height="510" alt="Кот">
+        </div>
+      </div>
+    </header>
+    
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
