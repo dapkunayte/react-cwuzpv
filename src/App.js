c87535461e273e8649f229bd82e50b1fb906c79d
dapkunayte <gladiatormahotina@yandex.ru>
2021-04-06 11:01:39 +0700

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import "./style.css";
 export default function App() {
   return (
     <div>
+    <html lang="ru"></html>
+    <head>
+    <meta charset="utf-8"></meta>
+    <title>Привет всем присутствующим</title>
+    <link rel="stylesheet" href="style.css"></link>
+    <link rel="shortcut icon" src="cat_icon.png"></link>
+    <meta name="viewport" content="width=device-width"></meta>
+    </head>
+
     <header class="site-header">
       <div class="container">
         <h1>УЧИМСЯ ВЕРСТАТЬ САЙТЫ</h1>
         <div class="imgpos">
-        <img class="cat" src="cat2.png" width="510" height="510" alt="Кот"></img>
+        <img class="cat" src="imgs/cat2.png" width="510" height="510" alt="Кот"></img>
         </div>
       </div>
     </header>
 
         <div class="info">
-    <img class="back" src="Frame.png"></img>
+    <img class="back" src="imgs/Frame.png"></img>
     <a target="_blank" class="button" href="https://yandex.ru/images/search?from=tabbar&text=рыжие%20коты">БОЛЬШЕ</a>
     </div>
 
